7f4703e1981502c2fad1dbf7cac2540561bfb641
Heerco_Grond <heercogrond@live.nl>
2018-11-29 13:10:31 +0100

    Rebuilding the playground

--- VoxosonusMLSuite/CreateVoxosonusModel.playground/Contents.swift
+++ VoxosonusMLSuite/CreateVoxosonusModel.playground/Contents.swift
@@ -1,3 +1,38 @@
-import Cocoa
+import Foundation
+import CreateML
 
-var str = "Hello, playground"
+
+// Input the name of the file you have imported into the resources of the playground.
+let datasetName = ""
+
+// The model's author name for the metadata.
+let author = "Voxosonus"
+// The short description for the model's metadata.
+let description = "A MLModel Generated using the tools of the Voxosonus Framework"
+// The version of the input model. This is for your own data record. Default is 1.0;
+let version = "1.0"
+
+
+
+
+// Get the current path to the document directory.
+let dirs = NSSearchPathForDirectoriesInDomains(.documentDirectory,
+                                               .userDomainMask, true)
+// And createa folder to save the model to.
+let currentDir = dirs[0] + "/VoxosonusModels/"
+let path = URL(fileURLWithPath: currentDir)
+
+let data = try MLDataTable(contentsOf: Bundle.main.url(forResource: "dataset", withExtension: "json")!)
+let (trainingData, testingData) = data.randomSplit(by: 0.7, seed: 5)
+let classifier = try MLTextClassifier(trainingData: trainingData, textColumn: "text", labelColumn: "label")
+
+let trainingAccuracy = (1.0 - classifier.trainingMetrics.classificationError) * 100;
+let validationAccuracy = (1.0 - classifier.validationMetrics.classificationError) * 100;
+
+let evaluationMetrics = classifier.evaluation(on: testingData)
+let evaluationAccuracy = (1.0 - evaluationMetrics.classificationError) * 100
+
+let metadata = MLModelMetadata(author: author, shortDescription: description, version: "1.0")
+
+try classifier.write(to: path)
+
