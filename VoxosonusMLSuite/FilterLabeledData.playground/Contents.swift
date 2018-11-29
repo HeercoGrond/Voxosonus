8b80251e11931effd7d627b7ccbd8f25cc6e1c0a
Heerco_Grond <heercogrond@live.nl>
2018-11-29 14:05:11 +0100

    Functional again

--- VoxosonusMLSuite/FilterLabeledData.playground/Contents.swift
+++ VoxosonusMLSuite/FilterLabeledData.playground/Contents.swift
@@ -1,3 +1,41 @@
-import Cocoa
+import Foundation
 
-var str = "Hello, playground"
+if let path = Bundle.main.path(forResource: "dataset", ofType: "json") {
+    do {
+        let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+        
+        print("doing thign")
+        let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+        let result = jsonResult as? [Any]
+        
+        //print(result)
+        
+        var newJson: [Any] = []
+        
+        for intentDict in result! {
+            if let dict = intentDict as? [String: Any] {
+                let label = dict["intent"] as! String
+                let utterances = dict["utterances"] as! [String]
+                
+                for utterance in utterances {
+                    let object: [String: Any] = [
+                        "label": label,
+                        "text": utterance
+                    ]
+                    
+                    newJson.append(object)
+                }
+            }
+        }
+        
+        print(newJson)
+        let valid = JSONSerialization.isValidJSONObject(newJson)
+        print(valid)
+        
+        
+    } catch {
+        print("crashed")
+        print(error)
+        // handle error
+    }
+}
