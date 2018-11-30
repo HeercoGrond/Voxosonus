c5ff760f44c5738532a8dcd61d96d4d59c0ba04f
Heerco_Grond <heercogrond@live.nl>
2018-11-30 18:45:28 +0100

    Saving of the new datafile

--- VoxosonusMLSuite/FilterLabeledData.playground/Contents.swift
+++ VoxosonusMLSuite/FilterLabeledData.playground/Contents.swift
@@ -1,5 +1,24 @@
 import Foundation
 
+let fileManager = FileManager.default;
+
+let documentsDirectoryPathString = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+let documentsDirectoryPath = NSURL(string: documentsDirectoryPathString)!
+
+let jsonFilePath = documentsDirectoryPath.appendingPathComponent("/dataset.json")
+var isDirectory: ObjCBool = false
+
+if !fileManager.fileExists(atPath: jsonFilePath!.absoluteString, isDirectory: &isDirectory) {
+    let created = fileManager.createFile(atPath: jsonFilePath!.absoluteString, contents: nil, attributes: nil)
+    if created {
+        print("File created ")
+    } else {
+        print("Couldn't create file for some reason")
+    }
+} else {
+    print("File already exists")
+}
+
 if let path = Bundle.main.path(forResource: "dataset", ofType: "json") {
     do {
         let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
@@ -28,9 +47,22 @@ if let path = Bundle.main.path(forResource: "dataset", ofType: "json") {
             }
         }
         
-        print(newJson)
         let valid = JSONSerialization.isValidJSONObject(newJson)
-        print(valid)
+        if(valid) {
+            let jsonData =  try JSONSerialization.data(withJSONObject: newJson, options: [])
+            let jsonString = String(data: jsonData, encoding: String.Encoding.utf8)
+            print(jsonString)
+            
+            do {
+                let file = try FileHandle(forWritingTo: jsonFilePath!)
+                file.write(jsonData)
+                print("JSON data was written to the file successfully!")
+            } catch let error as NSError {
+                print("Couldn't write to file: \(error.localizedDescription)")
+            }
+        }
+        
+        
         
         
     } catch {
