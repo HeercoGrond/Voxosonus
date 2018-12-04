ab5bec0e667532f7d6fa236cae72799ea3e6f3fe
Heerco_Grond <heercogrond@live.nl>
2018-12-04 11:54:36 +0100

    Fix Project because it was a mess

--- Framework/Voxosonus.framework/VoxosonusMLSuite/CreateVoxosonusModel.playground/Contents.swift
+++ Framework/Voxosonus.framework/VoxosonusMLSuite/CreateVoxosonusModel.playground/Contents.swift
@@ -1,3 +1,45 @@
-import Cocoa
+import Foundation
+import CreateML
 
-var str = "Hello, playground"
+// Input the name of the file you have imported into the resources of the playground.
+// Note: The expected dataset is to be in pairs of objects constructed as such:
+// {
+//  label: "",
+//  text: "",
+// }
+// This is because the MLTextClassifier does not support embedded arrays.
+let datasetName = "dataset"
+
+// Change this if you wish to use other definitions for the properties of the objects. These are default
+let datasetLabelName = "label"
+let datasetTextName = "text"
+
+// The model's author name for the metadata.
+let author = "Voxosonus"
+// The short description for the model's metadata.
+let description = "A MLModel Generated using the tools of the Voxosonus Framework"
+// The version of the input model. This is for your own data record. Default is 1.0;
+let version = "1.0"
+
+
+// Get the current path to the document directory.
+let dirs = NSSearchPathForDirectoriesInDomains(.documentDirectory,
+                                               .userDomainMask, true)
+// And createa folder to save the model to.
+let currentDir = dirs[0] + "/VoxosonusModels/"
+let path = URL(fileURLWithPath: currentDir)
+
+let data = try MLDataTable(contentsOf: Bundle.main.url(forResource: datasetName, withExtension: "json")!)
+let (trainingData, testingData) = data.randomSplit(by: 0.7, seed: 5)
+let classifier = try MLTextClassifier(trainingData: trainingData, textColumn: datasetTextName, labelColumn: datasetLabelName)
+
+let trainingAccuracy = (1.0 - classifier.trainingMetrics.classificationError) * 100;
+let validationAccuracy = (1.0 - classifier.validationMetrics.classificationError) * 100;
+
+let evaluationMetrics = classifier.evaluation(on: testingData)
+let evaluationAccuracy = (1.0 - evaluationMetrics.classificationError) * 100
+
+let metadata = MLModelMetadata(author: author, shortDescription: description, version: "1.0")
+
+try classifier.write(to: path)
+
--- Framework/Voxosonus.framework/VoxosonusMLSuite/FilterLabeledData.playground/Contents.swift
+++ Framework/Voxosonus.framework/VoxosonusMLSuite/FilterLabeledData.playground/Contents.swift
@@ -1,3 +1,73 @@
-import Cocoa
+import Foundation
 
-var str = "Hello, playground"
+let fileManager = FileManager.default;
+
+let documentsDirectoryPathString = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+let documentsDirectoryPath = NSURL(string: documentsDirectoryPathString)!
+
+let jsonFilePath = documentsDirectoryPath.appendingPathComponent("/dataset.json")
+var isDirectory: ObjCBool = false
+
+if !fileManager.fileExists(atPath: jsonFilePath!.absoluteString, isDirectory: &isDirectory) {
+    let created = fileManager.createFile(atPath: jsonFilePath!.absoluteString, contents: nil, attributes: nil)
+    if created {
+        print("File created ")
+    } else {
+        print("Couldn't create file for some reason")
+    }
+} else {
+    print("File already exists")
+}
+
+if let path = Bundle.main.path(forResource: "dataset", ofType: "json") {
+    do {
+        let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+        
+        print("doing thign")
+        let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+        let result = jsonResult as? [Any]
+        
+        //print(result)
+        
+        var newJson: [Any] = []
+        
+        for intentDict in result! {
+            if let dict = intentDict as? [String: Any] {
+                let label = dict["intent"] as! String
+                let utterances = dict["utterances"] as! [String]
+                
+                for utterance in utterances {
+                    let object: [String: Any] = [
+                        "label": label,
+                        "text": utterance
+                    ]
+                    
+                    newJson.append(object)
+                }
+            }
+        }
+        
+        let valid = JSONSerialization.isValidJSONObject(newJson)
+        if(valid) {
+            let jsonData =  try JSONSerialization.data(withJSONObject: newJson, options: [])
+            let jsonString = String(data: jsonData, encoding: String.Encoding.utf8)
+            print(jsonString)
+            
+            do {
+                let file = try FileHandle(forWritingTo: jsonFilePath!)
+                file.write(jsonData)
+                print("JSON data was written to the file successfully!")
+            } catch let error as NSError {
+                print("Couldn't write to file: \(error.localizedDescription)")
+            }
+        }
+        
+        
+        
+        
+    } catch {
+        print("crashed")
+        print(error)
+        // handle error
+    }
+}
